2fce5004fc3acdd2852259532c7b3648945787e4
Radadeli <ramirodelillo@gmail.com>
2023-11-20 12:25:31 +0100

    ejercicio 42 finalizado

---
+++ Exercise 42 - Local Storage/exercise42.js
@@ -0,0 +1,19 @@
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+function saveUser (userObject) {
+  try {
+    const userJSON = JSON.stringify(userObject)
+    localStorage.setItem('user', userJSON)  //Estos son los dos argumentos, clave(user) y valor (userJSON)
+    console.log('User save in localStorage')
+  }
+    catch(error) {
+      console.error('Error', error);
+    }
+ 
+}
+
+saveUser();
